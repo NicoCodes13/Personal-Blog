a783cc6c7de32e57f6d984977a357e26b938d9fc
Nico Castañeda <nickcar13@gmail.com>
2021-05-11 11:52:43 -0500

    :art: Creating header template

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -1,6 +1,22 @@
 const Header = () => {
   const view = /*html*/ `
-    <h1>Hello this is my header</h1>
+  <section>
+    <div>
+      <a href="">Facebook</a>
+      <a href="">Twitter</a>
+      <a href="">Instagram</a>
+      <a href="">Linkedin</a>
+      <a href="">Youtube</a>
+    </div>
+  </section>
+  <nav>
+    <section>
+      <a href="/">Logo</a>
+    </section>
+    <section>
+      <a href="#/porfile">Sobre mí</a>
+    </section>
+  </nav>
   `;
   return view;
 };
