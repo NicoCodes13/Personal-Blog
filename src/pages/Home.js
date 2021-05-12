27cc2131ed5be608cdc2f8e3c866a0f98c88f04f
Nico Castañeda <nickcar13@gmail.com>
2021-05-12 13:20:59 -0500

    :art: finish home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
+import '../styles/home.css';
 const Home = () => {
   const view = /*html*/ `
-  <main class='home-main'>
-    <section>
-      <h1 class ="home-main-text"> Conoce las novedades y <br> noticias del Mundo Tech</h1>
-      <a class="home-main-title" href="/#blogs"></a>
-    </section>
+  <main class="home-main">
+      <section>
+        <h1 class="home-main-text">
+          Conoce las novedades y <br />
+          noticias del Mundo Tech
+        </h1>
+        <a class="home-main-button" href="#/blogs">
+          Entra ya!
+        </a>
+      </section>
   </main>
   `;
   return view;
